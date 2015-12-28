bb66f7d09a23b20c7a50b3508a3c28ca810f751b
bug1024 <bug_1024@163.com>
2015-12-28 21:47:40 +0800

    multi app

--- index.php
+++ index.php
@@ -12,19 +12,24 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'ON');
 
 // constants
-define('CORE_PATH', __DIR__ . '/core/');
-define('CONFIG_PATH', __DIR__ . '/config/');
-define('APP_PATH', __DIR__ . '/app/');
-define('VIEW_PATH', __DIR__ . '/view/');
-
+define('CONF_PATH', __DIR__ . '/config/');
+define('COMMON_CONFIG_PATH', CONF_PATH. 'common/');
 // env
-require CONFIG_PATH . 'env.php';
+require CONF_PATH . 'env.php';
+define('CONFIG_PATH', CONF_PATH . ENV . '/');
+define('CORE_PATH', __DIR__ . '/core/');
+define('API_PATH', __DIR__ . '/api/');
+define('VIEW_PATH', __DIR__ . '/view/');
+define('APP_PATH', __DIR__ . '/app/');
+define('APP1_PATH', __DIR__ . '/app1/');
+
 
 // autoload
 require CORE_PATH . 'Loader.php';
 (new Loader)
     ->addNamespace('core', CORE_PATH)
     ->addNamespace('app', APP_PATH)
+    ->addNamespace('app1', APP1_PATH)
     ->register();
 
 try {
