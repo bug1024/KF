d8b393c105e42d3a866c48514911899fa0a40493
bug1024 <bug1024.com@gmail.com>
2017-02-24 22:38:39 +0800

    Validator

---
+++ core/validate/Validator.php
@@ -0,0 +1,18 @@
+<?php namespace core\validate;
+
+class Validator {
+
+    public static function isPhoneNumber($phone) {
+    }
+
+    public static function isEmail($email) {
+    }
+
+    public static function isIp($ip) {
+    }
+
+    public static function isIdCard($id_card) {
+    }
+
+}
+
