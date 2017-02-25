97a99a5f963b85d41aacfe24d8554cd72bd38cb6
bug1024 <bug1024.com@gmail.com>
2017-02-25 18:14:44 +0800

    ArrayUtil

--- core/util/ArrayUtil.php
+++ core/util/ArrayUtil.php
@@ -2,6 +2,36 @@
 
 class ArrayUtil {
 
+    public static function get($arr, $key, $default = false) {
+        return isset($arr[$key]) ? $arr[$key] : $default;
+    }
+
+    public static function toHash($arr, $key) {
+        $hash = [];
+        foreach ($arr as $value) {
+            if (isset($value[$key])) {
+                $hash[$value[$key]] = $value;
+            }
+        }
+
+        return $hash;
+    }
+
+    public static function getCols($arr, $key, $unique = true) {
+        $cols = [];
+        foreach ($arr as $value) {
+            if (isset($value[$key])) {
+                $cols[] = $value[$key];
+            }
+        }
+
+        if ($unique) {
+            $cols = array_unique($cols);
+        }
+
+        return $cols;
+    }
+
 
 }
 
