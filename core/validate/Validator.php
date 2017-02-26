70d53e2e49490fcdc95988ffce626112adab0df6
bug1024 <bug1024.com@gmail.com>
2017-02-26 21:07:57 +0800

    validate

--- core/validate/Validator.php
+++ core/validate/Validator.php
@@ -2,16 +2,23 @@
 
 class Validator {
 
-    public static function isPhoneNumber($phone) {
+    public static function isPhoneNumber($value) {
+        return (boolean) preg_match('/^1\d{10}$/', $value);
     }
 
-    public static function isEmail($email) {
+    public static function isEmail($value) {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 
-    public static function isIp($ip) {
+    public static function isIp($value) {
+        return filter_var($value, FILTER_VALIDATE_IP);
     }
 
-    public static function isIdCard($id_card) {
+    public static function isIdCard($value) {
+    }
+
+    public static function isUrl($value) {
+        return filter_var($value, FILTER_VALIDATE_URL);
     }
 
 }
